e43032f7e17dfe4fa41b680c64b4d01a0dc49a40
Maggie <maj2179@columbia.edu>
2022-05-19 09:07:33 -0400

    updating route

--- mixedMessages/script.js
+++ mixedMessages/script.js
@@ -3,3 +3,11 @@ let time = d.getTime(); // returns num ms since jan 1, 1970
 console.log(time);
 const lastDigitStr = String(time).slice(-1);  //gets last digit of the time
 console.log(lastDigitStr);
+
+// script will determine how to get to school
+
+const totalRoute = {
+    modeOfTransport: ['bike', 'train', 'bus', 'walk'],
+    leaveAt: ['7:30', '8:00', '9:00'],
+    getSnack: ['Dunkin\'', 'Starbucks', 'Matto']
+}
