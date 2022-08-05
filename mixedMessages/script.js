bde0f0b2f1a8b489a82f289b7d53756705cd7afc
Maggie <maj2179@columbia.edu>
2022-08-04 20:41:59 -0400

    object properties, getting back into it

--- mixedMessages/script.js
+++ mixedMessages/script.js
@@ -1,8 +1,6 @@
 const d = new Date();
 let time = d.getTime(); // returns num ms since jan 1, 1970
-console.log(time);
 const lastDigitStr = String(time).slice(-1);  //gets last digit of the time
-console.log(lastDigitStr);
 
 // script will determine how to get to school
 
@@ -20,5 +18,9 @@ let schoolRoute = []
 
 for(let choice in totalRoute){
     let ranNum = Math.floor(Math.random()*totalRoute[choice].length)
-    console.log(ranNum)
+    schoolRoute[choice] = totalRoute[choice][ranNum]
 }
+
+//console.log('to get to school, you will take the ' + schoolRoute[modeOfTransport] + ' and you will leave at ' + schoolRoute[leaveAt] + ' and you will eat ' + schoolRoute[getSnack])
+let newString = 'You will take the ' + schoolRoute.modeOfTransport + ', leaving at ' + schoolRoute.leaveAt + ' and you will get a snack at '+ schoolRoute.getSnack + '. You will do this ' + lastDigitStr + ' days in a row'
+console.log(newString)
